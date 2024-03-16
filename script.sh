e4326c72fcdd98d26ca625f6d7f8acd9c93abc9b
Vijay Giduthuri <125960600+vijaygiduthuri@users.noreply.github.com>
2024-03-16 20:04:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-echo "Hello Vijay Giduthuri" | sudo tee /var/www/html/index.html
+echo "Hello Vijay Giduthuri" | sudo tee /var/www/html/index.html 
